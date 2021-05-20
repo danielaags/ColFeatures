03444d72d2ad42b6dd5b72a73fd23bd3776fe074
Daniela Garcia <daniela.ags13@gmail.com>
2021-05-20 11:01:07 +0200

    Update matchColonies_Final.m
    License added

--- mainScripts/matchColonies_Final.m
+++ mainScripts/matchColonies_Final.m
@@ -1,8 +1,10 @@
 function[] = matchColonies_Final(folderpath, time)
+%Copyright (c) 2021, Daniela A. Garcia-Soriano
+%All rights reserved.
+
 %The function is desing to match colonies over time after having extracting
 %morphological characteristics from each time point. The tif data has to be
 %organised by plate and include the different time points to be used.
-%path = 'C:\Users\au648169\Documents\Postdoc_TorringLab\ImagingAnalysis\02-SecondRound\10%NBA(190exp)\PerPlate';
 
 %Last version 25.05.2020
 
@@ -159,3 +161,14 @@ for a = 1:length(folders)
 end    
 end
 
+%Licensed under the Apache License, Version 2.0 (the "License");
+%you may not use this file except in compliance with the License.
+%You may obtain a copy of the License at
+
+%    http://www.apache.org/licenses/LICENSE-2.0
+
+%Unless required by applicable law or agreed to in writing, software
+%distributed under the License is distributed on an "AS IS" BASIS,
+%WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%See the License for the specific language governing permissions and
+%limitations under the License.
